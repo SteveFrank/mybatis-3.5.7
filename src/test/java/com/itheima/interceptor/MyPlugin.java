96465de4cca3f5b39aa2fead27d43673935b9de1
FrankQian <yangqian3@staff.weibo.com>
2021-11-02 18:24:58 +0800

    first commit

---
+++ src/test/java/com/itheima/interceptor/MyPlugin.java
@@ -0,0 +1,70 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.itheima.interceptor;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.plugin.*;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+@Intercepts({
+    @Signature(type = StatementHandler.class,
+               method = "prepare",
+               args = {Connection.class,Integer.class})
+})
+public class MyPlugin implements Interceptor {
+
+  /**
+   * 拦截方法：每次执行目标方法时，都会进入到intercept方法中
+   * @param invocation ：多个参数的封装类
+   * @return
+   * @throws Throwable
+   */
+  @Override
+  public Object intercept(Invocation invocation) throws Throwable {
+    // 增强逻辑:将执行的sql进行记录（打印）
+    StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+    BoundSql boundSql = statementHandler.getBoundSql();
+    String sql = boundSql.getSql();
+
+    System.out.println("拦截方法，记录Sql：" + sql);
+
+    return invocation.proceed();
+  }
+
+  /**
+   * 将目标对象生成代理对象，添加到拦截器链中
+   * @param target ：目标对象
+   * @return
+   */
+  @Override
+  public Object plugin(Object target) {
+    // wrap 将目标对象，基于JDK动态代理生成代理对象
+    return Plugin.wrap(target,this);
+  }
+
+  /**
+   * 设置属性
+   * @param properties 插件初始化的时候，会设置的一些值的属性集合
+   */
+  @Override
+  public void setProperties(Properties properties) {
+    System.out.println("插件配置的初始化参数：" + properties);
+
+  }
+}
---
+++ src/test/java/com/itheima/test/CacheTest.java
@@ -0,0 +1,111 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.itheima.test;
+
+import com.itheima.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CacheTest {
+
+  /**
+   * 测试一级缓存
+   */
+  @Test
+  public void firstLevelCacheTest() throws IOException {
+
+    InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+
+    // 2. (1)解析了配置文件，封装configuration对象 （2）创建了DefaultSqlSessionFactory工厂对象
+    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+    // 3.问题：openSession()执行逻辑是什么？
+    // 3. (1)创建事务对象 （2）创建了执行器对象cachingExecutor (3)创建了DefaultSqlSession对象
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+
+    // 发起第一次查询，查询ID为1的用户
+    User user1 = sqlSession.selectOne("com.itheima.mapper.UserMapper.findByCondition", 1);
+
+    // 更新操作
+    User user = new User();
+    user.setId(1);
+    user.setUsername("tom");
+    sqlSession.update("com.itheima.mapper.UserMapper.updateUser",user);
+    sqlSession.commit();
+
+    // 发起第二次查询，查询ID为1的用户
+    User user2 = sqlSession.selectOne("com.itheima.mapper.UserMapper.findByCondition", 1);
+
+    System.out.println(user1 == user2);
+    System.out.println(user1);
+    System.out.println(user2);
+
+    sqlSession.close();
+
+
+  }
+
+
+  /**
+   * 测试二级缓存
+   */
+  @Test
+  public void secondLevelCacheTest() throws IOException {
+
+    InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+
+    // 2. (1)解析了配置文件，封装configuration对象 （2）创建了DefaultSqlSessionFactory工厂对象
+    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+    // 3.问题：openSession()执行逻辑是什么？
+    // 3. (1)创建事务对象 （2）创建了执行器对象cachingExecutor (3)创建了DefaultSqlSession对象
+    SqlSession sqlSession1 = sqlSessionFactory.openSession();
+    SqlSession sqlSession2 = sqlSessionFactory.openSession();
+
+    // 发起第一次查询，查询ID为1的用户
+    User user1 = sqlSession1.selectOne("com.itheima.mapper.UserMapper.findByCondition", 1);
+
+    // **必须要调用sqlSession的commit方法或者close方法，才能让二级缓存生效
+    sqlSession1.commit();
+
+    // 更新操作
+    SqlSession sqlSession3 = sqlSessionFactory.openSession();
+    User user = new User();
+    user.setId(1);
+    user.setUsername("tom");
+
+    sqlSession3.update("com.itheima.mapper.UserMapper.updateUser",user);
+    sqlSession3.commit();
+
+    // 第二次查询
+    User user2 = sqlSession2.selectOne("com.itheima.mapper.UserMapper.findByCondition", 1);
+
+    System.out.println(user1==user2);
+    System.out.println(user1);
+    System.out.println(user2);
+
+    sqlSession1.close();
+
+
+  }
+
+}
